eb77c4d1083cb770fdd2357503752e718f8aa2b9
ipolishc22 <148147752+ipolishc22@users.noreply.github.com>
2023-10-16 16:43:00 -0400

    Update git.py
    Adding another function to the script

--- git.py
+++ git.py
@@ -1,4 +1,8 @@
 # creating a script in the git repository 
 # that prints out "Hello world!"
-
 print("Hello world!")
+
+
+# pulling down previous changes and adding 
+# another function
+print("Goodbye world!")
